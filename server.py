d165f1ed3f334e93bdb8d464cedc8b32751467c8
Niyas Mohammed <niaz.angels@gmail.com>
2019-11-04 02:13:05 +0530

    WIP: server

--- server.py
+++ server.py
@@ -5,12 +5,12 @@ from base import ProtocolHandler, Server
 class RedisProtocol(ProtocolHandler):
     def __init__(self):
         self.handlers = {
-            "+": self("simple_string"),
-            "$": self("bulk_string"),
-            "-": self("error"),
-            ":": self("integer"),
-            "*": self("array"),
-            "%": self("dict"),
+            "+": self("_str"),
+            "$": self("_bulk_str"),
+            "-": self("_error"),
+            ":": self("_int"),
+            "*": self("_array"),
+            "%": self("_dict"),
         }
 
     def handle_request(self, socket_file):
@@ -18,12 +18,35 @@ class RedisProtocol(ProtocolHandler):
         if not first_byte:
             raise DisconnectError()
         try:
-            return self(first_byte)(socket_file)
+            return self.handlers[first_byte](socket_file)
         except KeyError:
-            raise CommandError("Bad request")
+            raise CommandError("Bad command")
 
-    def simple_string(self, x):
-        print("In simple_string!")
+    def _str(self, socket_file):
+        return socket_file.readline().rstrip("\r\n")
+
+    def _int(self, socket_file):
+        return socket_file.readline().rstrip("\r\n")
+
+    def _error(self, socket_file):
+        return Error(socket_file.readline().rstrip("\r\n"))
+
+    def _bulk_str(self, socket_file):
+        length = int(socket_file.readline().rstrip("\r\n"))
+        if length == -1:
+            return None  # Special case for Nulls
+        length += 2  # Include trailing '\r\n'. Is this necessary??
+        return socket_file.read(length)[:-2]
+
+    def _array(self, socket_file):
+        num_elements = int(socket_file.readine().rstrip("\r\n"))
+        return [self.handle_request(socket_file) for _ in range(num_elements)]
+
+    def _dict(self, socket_file):
+        num_elements = int(socket_file.readline().rstrip("\r\n"))
+        elements = [self.handle_request(socket_file) for _ in range(2 * num_elements)]
+        ks, vs = elements[::2], elements[1::2]
+        return dict(zip(ks, vs))
 
     def __call__(self, x):
         f = getattr(self, x, None)
@@ -34,5 +57,5 @@ class RedisProtocol(ProtocolHandler):
 
 if __name__ == "__main__":
     pro = RedisProtocol()
-    pro("simple_string")(12)
+    pro("_str")(12)
 
